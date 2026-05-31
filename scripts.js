7fce8ee5ee2d0fb7c81190bccc52c6cd55758f0a
Fox <LindaFuchs@users.noreply.github.com>
2026-05-31 20:32:04 +0200

    Initial commit from W3Schools

---
+++ scripts.js
@@ -0,0 +1,64 @@
+//Season Selection Functions
+
+//Display the season selection overview
+/*function displaySeasonSelection(){
+    document.getElementById("season").innerHTML = "<img id=\"foggy\" src=\"/images/seasons/foggy.png\" alt=\"foggy\">";
+}*/
+
+//Display summer image
+function displaySummer(){
+    document.getElementById("season").innerHTML ="<img id=\"summer\" src=\"/images/seasons/summer.png\" alt=\"summer\">";
+}
+
+//Display spring image
+function displaySpring(){
+    document.getElementById("season").innerHTML ="<img id=\"spring\" src=\"/images/seasons/spring.png\" alt=\"spring\">";
+}
+
+//Display fall image
+function displayFall(){
+    document.getElementById("season").innerHTML ="<img id=\"fall\" src=\"/images/seasons/fall.png\" alt=\"foggy\">";
+}
+
+//Display winter image
+function displayWinter(){
+    document.getElementById("season").innerHTML ="<img id=\"winter\" src=\"/images/seasons/winter.png\" alt=\"winter\">";
+}
+
+
+//Weather Selection Functions
+
+//Display the weather selection overview
+function displayWeatherSelection(){
+    document.getElementById("weather").innerHTML ="<>";
+}
+
+//Display sunny image
+function displaySunny(){
+    document.getElementById("weather").innerHTML ="<img id=\"sunny\" src=\"/images/weather/sunny.png\" alt=\"sunny\">";
+}
+
+//Display foggy image
+function displayFoggy(){
+    document.getElementById("weather").innerHTML ="<img id=\"foggy\" src=\"/images/weather/foggy.png\" alt=\"foggy\">";
+}
+
+//Display partially_clouded image
+function displayPartiallyClouded(){
+    document.getElementById("weather").innerHTML ="<img id=\"partially_clouded\" src=\"/images/weather/partially_clouded.png\" alt=\"partially_clouded\">";
+}
+
+//Display snowing image
+function displaySnowing(){
+    document.getElementById("weather").innerHTML ="<img id=\"snowing\" src=\"/images/weather/snowing.png\" alt=\"snowing\">";
+}
+
+//Display clouded image
+function displayClouded(){
+    document.getElementById("weather").innerHTML ="<img id=\"clouded\" src=\"/images/weather/clouded.png\" alt=\"clouded\">";
+}
+
+//Display rain image
+function displayRaining(){
+    document.getElementById("weather").innerHTML ="<img id=\"raining\" src=\"/images/weather/raining.png\" alt=\"raining\">";
+}
